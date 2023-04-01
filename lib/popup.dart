b49a034bb71f35a22b077da54d6ef67b31c9b95d
hyunmila <kamilakopacz@student.agh.edu.pl>
2023-04-02 00:46:10 +0200

    popup

--- lib/popup.dart
+++ lib/popup.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 
@@ -11,31 +13,46 @@ class Popup extends StatefulWidget {
 }
 
 class _PopupState extends State<Popup> {
-  final List<IconData> _icons = [
-    Icons.star_border,
-    Icons.star_half,
-    Icons.star
-  ];
-  int _currentIcon = 0;
+  // final List<IconData> _icons = [
+  //   Icons.star_border,
+  //   Icons.star_half,
+  //   Icons.star
+  // ];
+  // int _currentIcon = 0;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: InkWell(
-        onTap: () => setState(() {
-          _currentIcon = (_currentIcon + 1) % _icons.length;
-        }),
+      child: 
+      
+        Container(
+        constraints: const BoxConstraints(minWidth: 200, maxWidth: 200, minHeight: 50, maxHeight: 100),
         child: Row(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 10),
-              child: Icon(_icons[_currentIcon]),
-            ),
+          // Padding(
+          //     padding: const EdgeInsets.only(left: 20, right: 10),
+          //     // child: Icon(_icons[_currentIcon]),
+          //   ),
             _cardDescription(context),
-          ],
-        ),
-      ),
+        ]),
+        )
+      
+      // InkWell(
+      //   onTap: () => setState(() {
+      //     _currentIcon = (_currentIcon + 1) % _icons.length;
+      //   }),
+      //   child: Row(
+      //     mainAxisSize: MainAxisSize.min,
+      //     children: <Widget>[
+      //       Padding(
+      //         padding: const EdgeInsets.only(left: 20, right: 10),
+      //         child: Icon(_icons[_currentIcon]),
+      //       ),
+      //       _cardDescription(context),
+      //     ],
+      //   ),
+      // ),
     );
   }
 
@@ -43,14 +60,14 @@ class _PopupState extends State<Popup> {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: Container(
-        constraints: const BoxConstraints(minWidth: 100, maxWidth: 200),
+        constraints: const BoxConstraints(minWidth: 100,),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             const Text(
-              'Popup for a marker!',
+              'Place name',
               overflow: TextOverflow.fade,
               softWrap: false,
               style: TextStyle(
@@ -59,14 +76,29 @@ class _PopupState extends State<Popup> {
               ),
             ),
             const Padding(padding: EdgeInsets.symmetric(vertical: 4.0)),
-            Text(
-              'Position: ${widget.marker.point.latitude}, ${widget.marker.point.longitude}',
-              style: const TextStyle(fontSize: 12.0),
-            ),
-            Text(
-              'Marker size: ${widget.marker.width}, ${widget.marker.height}',
-              style: const TextStyle(fontSize: 12.0),
-            ),
+            // Text(
+            //   // 'Position: ${widget.marker.point.latitude}, ${widget.marker.point.longitude}',
+            //   'Add to visited places',
+            //   style: const TextStyle(fontSize: 12.0)),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              IconButton(
+                tooltip: 'Add to visited places',
+                onPressed: (){
+
+              }, icon: Icon(Icons.add_box_outlined)),
+              IconButton(
+                tooltip: 'Add to favourite places',
+                onPressed: (){
+
+              }, icon: Icon(Icons.favorite)),
+            ],),
+              
+            // Text(
+            //   'Add to favourite places',
+            //   style: const TextStyle(fontSize: 12.0),
+            // ),
           ],
         ),
       ),
