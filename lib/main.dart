62f6a3b122ba23b22b934e10f0f947c8992256d9
Miłosz Łagan <4965090+FireBite@users.noreply.github.com>
2023-04-01 13:22:49 +0200

    Firebase integration scaffolding

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:developer';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final storage = FirebaseStorage.instance;
+  final db = FirebaseFirestore.instance;
+
+  await db.collection("poi").get().then((event) {
+    for (var doc in event.docs) {
+      log("${doc.id} => ${doc.data()}");
+    }
+  });
+
   runApp(const MyApp());
 }
 
